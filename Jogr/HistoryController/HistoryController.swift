15160de0a424e869627e4570b80909b7a9a016fb
Mike Ferris <mferris@apple.com>
2017-05-07 08:14:08 -0700

    Add some stuff

---
+++ Jogr/HistoryController/HistoryController.swift
@@ -0,0 +1,9 @@
+//
+//  HistoryController.swift
+//  Jogr
+//
+//  Created by Mike Ferris on 5/6/17.
+//  Copyright © 2017 ToolsQA. All rights reserved.
+//
+
+import Foundation
---
+++ Jogr/RunController/MapAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  MapAnnotation.swift
+//  Jogr
+//
+//  Created by Mike Ferris on 5/6/17.
+//  Copyright © 2017 ToolsQA. All rights reserved.
+//
+
+import Foundation
